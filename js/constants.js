d3627df15289dfeca62442df5b4a33f3a0347a77
ankj89 <51509357+ankj89@users.noreply.github.com>
2026-06-13 13:41:47 +0530

    Create constants.js

---
+++ js/constants.js
@@ -0,0 +1,9 @@
+const DB_NAME = "RateAnalysisDB";
+const DB_VERSION = 1;
+
+const STORES = {
+    MATERIALS: "MaterialMaster",
+    LABOR: "LaborMaster",
+    SETTINGS: "Settings",
+    DERIVED: "DerivedRates"
+};
