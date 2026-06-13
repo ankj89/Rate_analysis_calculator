c567fcaed8b527711062508fa5c79d366651055b
ankj89 <51509357+ankj89@users.noreply.github.com>
2026-06-13 13:47:24 +0530

    Create materialService.js

---
+++ js/materialService.js
@@ -0,0 +1,79 @@
+const materialService = {
+
+    async getAll() {
+
+        return new Promise((resolve, reject) => {
+
+            const tx =
+                db.transaction(
+                    STORES.MATERIALS,
+                    "readonly"
+                );
+
+            const store =
+                tx.objectStore(
+                    STORES.MATERIALS
+                );
+
+            const request =
+                store.getAll();
+
+            request.onsuccess = () =>
+                resolve(request.result);
+
+            request.onerror = reject;
+
+        });
+
+    },
+
+    async addMany(records) {
+
+        return new Promise((resolve, reject) => {
+
+            const tx =
+                db.transaction(
+                    STORES.MATERIALS,
+                    "readwrite"
+                );
+
+            const store =
+                tx.objectStore(
+                    STORES.MATERIALS
+                );
+
+            records.forEach(r => store.add(r));
+
+            tx.oncomplete = resolve;
+            tx.onerror = reject;
+
+        });
+
+    },
+
+    async clear() {
+
+        return new Promise((resolve, reject) => {
+
+            const tx =
+                db.transaction(
+                    STORES.MATERIALS,
+                    "readwrite"
+                );
+
+            const store =
+                tx.objectStore(
+                    STORES.MATERIALS
+                );
+
+            const request =
+                store.clear();
+
+            request.onsuccess = resolve;
+            request.onerror = reject;
+
+        });
+
+    }
+
+};
